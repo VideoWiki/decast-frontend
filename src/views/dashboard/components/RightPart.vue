3c32d23e9aa9c0d0c01bab348b74ff99ca93e28a
Sujata Delu <sujatadelu@Sujatas-MacBook-Air.local>
2023-09-30 17:47:54 +0530

    delete confirmation popup

--- src/views/dashboard/components/RightPart.vue
+++ src/views/dashboard/components/RightPart.vue
@@ -156,10 +156,29 @@
                   <button>
                     <img src="@/assets/images/prepone.svg" alt="" />Prepone cast
                   </button>
-                  <button @click="deleteCast(cast.public_meeting_id, index)">
+                  <button @click="openDeletePopup()">
                     <img src="@/assets/images/delete.svg" />
                     Delete
                   </button>
+                  <!-- Delete Confirmation Popup -->
+                  <div v-if="showDeletePopup" class="delete-popup">
+                    <div class="full-wrapper">
+                      <div class="uppar-part flex justify-between">
+                        <div class="heading">Delete cast</div>
+                        <img src="@/assets/images/create-event/Vector30.svg" @click="closeDeletePopup" />
+                      </div>
+                      <div class="middle-part">
+                        <div class="img">
+                          <img src="@/assets/images/create-event/crossIcon.svg" />
+                        </div>
+                        <p>Do you really want to delete</p>
+                        <p>this cast?</p>
+                      </div>
+                      <div class="lower-part">
+                        <button @click="deleteCast(cast.public_meeting_id, index)">Confirm</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div class="inner-child3">
                   <button v-if="cast.is_running === 'true'" class="live-btn">
@@ -272,6 +291,7 @@ export default {
   name: 'rightpart',
   data() {
     return {
+      showDeletePopup: false,
       focusYourRooms: true,
       create: false,
       showCastIsLive: false,
@@ -295,6 +315,12 @@ export default {
     },
   },
   methods: {
+    openDeletePopup() {
+      this.showDeletePopup = true;
+    },
+    closeDeletePopup() {
+      this.showDeletePopup = false;
+    },
     closeAllPopups(e) {
       if (e.currentTarget === e.target) {
         this.create = false;
@@ -664,4 +690,75 @@ export default {
   background: #0000007e;
   z-index: 100;
 }
+</style>
+<style scoped>
+*:not(i) {
+  font-family: 'Karla', sans-serif;
+}
+.delete-popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999; /* Ensure it's on top of other content */
+}
+.full-wrapper{
+    Width : 278px;
+    Height : 212px;
+    background-color: #1F272F;
+    border-radius: 10px;
+    border: 1px solid #31394E;
+    padding: 14px 26px;
+    display: flex;
+    flex-direction: column;
+    
+}
+.heading{
+    font-size: 14px;
+    font-weight: 600;
+    color: #A6A6A8;
+}
+.uppar-part img{
+   height: 12px;
+   width: 12px;
+   margin-right: -18px;
+   margin-top: -6px;
+}
+.middle-part{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 25px;
+}
+.img{
+    margin: auto;
+}
+.middle-part p{
+    margin: auto;
+    font-size: 13px;
+    font-weight: 500;
+    color: #A6A6A8;
+}
+.lower-part{
+    align-self: flex-end;
+    margin-top: 33px;
+}
+.lower-part button{
+    width: 86px;
+    height: 26px;
+    border: 1px solid rgba(49, 57, 78, 0.6);
+    background-color: rgba(49, 57, 78, 0.6);
+    border-radius: 6px;
+    font-size: 12px;
+    font-weight: 500;
+    color: #A6A6A8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
 </style>
